06fe12990dee70dfc47935667349d56567a20916
DHRUV TILVA <tilvadhruv8@gmail.com>
2022-07-26 20:31:05 +0530

    my car website

---
+++ js/script.js
@@ -0,0 +1,183 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+document.querySelector('#login-btn').onclick = () =>{
+  document.querySelector('.login-form-container').classList.toggle('active');
+}
+
+document.querySelector('#close-login-form').onclick = () =>{
+  document.querySelector('.login-form-container').classList.remove('active');
+}
+
+window.onscroll = () =>{ 
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 0){
+    document.querySelector('.header').classList.add('active');
+  }else{
+    document.querySelector('.header').classList.remove('active');
+  };
+
+};
+
+document.querySelector('.home').onmousemove = (e) =>{
+
+  document.querySelectorAll('.home-parallax').forEach(elm =>{
+
+    let speed = elm.getAttribute('data-speed');
+
+    let x = (window.innerWidth - e.pageX * speed) / 90;
+    let y = (window.innerHeight - e.pageY * speed) / 90;
+
+    elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
+
+  });
+
+};
+
+
+document.querySelector('.home').onmouseleave = (e) =>{
+
+  document.querySelectorAll('.home-parallax').forEach(elm =>{
+
+    elm.style.transform = `translateX(0px) translateY(0px)`;
+
+  });
+
+};
+
+function callvalue(){
+  var n= document.getElementById("uname").value;
+  var email= document.getElementById("uemail").value;
+  var uissue= document.getElementById("issue").value;
+  var ulocation= document.getElementById("location").value;
+  document.writeln("<i>Your Pickup Vehicle Details:  <br><br><br>" + "            Name: <b>"+n+"<br>          ");
+  document.writeln(" Your Email is: <b>"+email+ "<br>");
+  document.writeln("Your issue of vehicle is: <b>"+uissue+ "<br>");
+  document.writeln("Your Location is: <b>"+ulocation+ "<br>");
+
+};
+
+
+function reminder(){
+  var ureminder= document.getElementById("userreminder").value;
+  document.writeln("Your Reminder To Get Quick Update Email is: <br> <b>" +ureminder+ "<b>");
+
+};
+ function carcall(){
+  var cardetail= document.getElementById("addcar").value;
+  document.writeln("Car Details has been Not Added Yet! This Feature is Available Soon!")
+ }
+var swiper = new Swiper(".vehicles-slider", {
+  grabCursor: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".featured-slider", {
+  grabCursor: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".review-slider", {
+  grabCursor: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// const element = document.querySelector('.header #menu-btn .fas fa-bars')
+// element.classList.add('animated', 'bounceOutLeft')
+
+// var swiper = new Swiper(".overview-slider", {
+//   grabCursor: true,
+//   centeredSlides: true,  
+//   spaceBetween: 20,
+//   loop:true,
+//   autoplay: {
+//     delay: 9500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable:true,
+//   },
+//   breakpoints: {
+//     0: {
+//       slidesPerView: 1,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//     },
+//     1024: {
+//       slidesPerView: 3,
+//     },
+//   },
+// });
